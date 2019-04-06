596e87e38998c7a2dfa1d953145cb4b234b84298
Michal Piekarczyk <namoopsoo>
2019-04-06 19:04:27 -0400

    draftings

--- conquer/src/conquer/meh.clj
+++ conquer/src/conquer/meh.clj
@@ -62,21 +62,40 @@
 
 (defn new? [position memory]
   (= #{}
-     (cljset/intersection (set position) (set memory))  
+     (cljset/intersection (set [position]) (set memory))  
      )
   )
 
-(defn combine [s1 s2 s3 s4]
+(defn combine [colls]
+  (prn "colls, " colls ".")
 
   (apply cljset/union
          (filter #(not (nil? %))
-                 [s1 s2 s3 s4]
+                 colls
                               )))
+
+(defn what-next [position land memory]
+  (println "\nDEBUG, memory, " memory)
+  (prn "DEBUG, position, " position)
+  (let [options (map #(% position land)
+                     [up down right left])
+        ; take out if in memory already though.
+        _ (prn "DEBUG, options, " options)
+        widdled (filter #(new? % memory) options)
+        _ (prn "DEBUG, widdled, " widdled)
+
+        ]
+    widdled
+
+    ))
 
 (defn conquer
   [position land mycolor memory]
 
   ; Went off grid
+  (println "\nconquer")
+  (println "pos. " position )
+  (println "memory. " memory )
   (if (nil? position)
     memory
 
@@ -84,25 +103,29 @@
              (new? position memory))
 
       ;
-      (let [capture-vec 
-            (map #(conquer (% position land)
+      (let [next-positions (what-next position land memory)
+            _ (prn "DEBUG, next:" next-positions)
+            
+            capture-vec 
+            (map #(conquer %
                            land
                            mycolor
 
                            ;accumulate memory 
                            (conj memory position)
                            )
-                 [up down right left])
-
+                 next-positions)
+            _ (prn "DEBUG." (count capture-vec))
+            _ (prn "DEBUG. capture-vec, " capture-vec)
+            together (combine capture-vec)
             ]
-        (println (str "DEBUG ... " ) capture-vec )
+        (prn (str "DEBUG ... " ) together)
 
-        (combine capture-vec)
+        
         )
       
       ;
       memory)
-
     ))
 
 
